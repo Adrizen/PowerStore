bbf01a57ed30e228f9684350ea705b4d4565219a
Adrizen <36826976+Adrizen@users.noreply.github.com>
2023-02-06 21:47:07 -0300

    local images support

--- PowerStore/PowerStore/urls.py
+++ PowerStore/PowerStore/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from ordersManagement import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls), # Admin panel
@@ -27,3 +29,5 @@ urlpatterns = [
     path('search/', views.search, name="search"),
     path('contact/', views.contact, name="contact"),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
